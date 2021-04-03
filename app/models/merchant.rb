f536e2d604de95bdfe6731c83bc051c637088720
Arika Turing <ablenker@gmail.com>
2021-04-03 13:59:22 -0600

    Add passing code for merchant validations test

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -4,4 +4,6 @@ class Merchant < ApplicationRecord
   has_many :invoice_items, through: :invoices
   has_many :customers, through: :invoices
   has_many :transactions, through: :invoices
+
+  validates_presence_of :name
 end
