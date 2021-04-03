30ef5e274bd2e45e4ba5fb8bd5e19318c9f6c4dc
Arika Turing <ablenker@gmail.com>
2021-04-03 13:58:18 -0600

    Add passing code for Item validation test

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -8,4 +8,10 @@ RSpec.describe Item, type: :model do
     it { should have_many(:customers).through(:invoices)}
     it { should have_many(:transactions).through(:invoices)}
   end
+
+  describe 'validations' do
+    it { should validate_presence_of :name}
+    it { should validate_presence_of :description}
+    it { should validate_presence_of :unit_price}
+  end
 end
