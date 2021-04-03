f92d5a98ac14c5848c53d382178c0a88e5c7f3d4
Arika Turing <ablenker@gmail.com>
2021-04-03 13:52:58 -0600

    Add passing code for Customer validations test

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -4,4 +4,6 @@ class Customer < ApplicationRecord
   has_many :invoice_items, through: :invoices
   has_many :items, through: :invoice_items
   has_many :merchants, through: :items
+
+  validates_presence_of :first_name, :last_name
 end
