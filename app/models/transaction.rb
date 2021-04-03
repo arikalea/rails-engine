f05df8b33b9491a3436ed8d830c446739816d112
Arika Turing <ablenker@gmail.com>
2021-04-03 13:46:03 -0600

    Add passing code for Transaction relaionship test

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,3 +1,7 @@
 class Transaction < ApplicationRecord
-  belongs_to :invoice_id
+  belongs_to :invoice
+  has_one :customer, through: :invoice
+  has_many :invoice_items, through: :invoice
+  has_many :items, through: :invoice_items
+  has_many :merchants, through: :items
 end
