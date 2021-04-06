a54345fef5f49f74180cbaa716d5f56ab363cb51
Arika Turing <ablenker@gmail.com>
2021-04-05 18:15:49 -0600

    Add test ad passing code for one merchant happy path

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -1,12 +1,10 @@
-# frozen_string_literal: true
+class Api::V1::MerchantsController < ApplicationController
+  def index
+    merchants = MerchantFacade.all_merchants(params.fetch(:page, 1), params.fetch(:per_page, 20))
+    render json: MerchantSerializer.new(merchants)
+  end
 
-module Api
-  module V1
-    class MerchantsController < ApplicationController
-      def index
-        merchants = MerchantFacade.all_merchants(params.fetch(:page, 1), params.fetch(:per_page, 20))
-        render json: MerchantSerializer.new(merchants)
-      end
-    end
+  def show
+    render json: Merchant.find(params[:id])
   end
 end
