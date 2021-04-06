0266e6b4354e357d0c3dc358973e3c4a6c99f91b
Arika Turing <ablenker@gmail.com>
2021-04-06 14:20:10 -0600

    Adjust all items happy path test

--- spec/requests/api/v1/all_items_spec.rb
+++ spec/requests/api/v1/all_items_spec.rb
@@ -8,12 +8,20 @@ RSpec.describe "All items API" do
       get '/api/v1/items'
 
       items = JSON.parse(response.body, symbolize_names: true)
-
+      # require "pry";binding.pry
       expect(response).to be_successful
       expect(response.status).to eq(200)
-      expect(items[:data].count).to eq(20)
-      # expect(items[:data].first).to have_key(:id)
-      # expect(items[:data].first[:id]).to be_a String
-    end   
+      expect(items.count).to eq(20)
+      expect(items.first).to have_key(:id)
+      expect(items.first[:id]).to be_a Integer
+      expect(items.first).to have_key(:name)
+      expect(items.first[:name]).to be_a String
+      expect(items.first).to have_key(:description)
+      expect(items.first[:description]).to be_a String
+      expect(items.first).to have_key(:unit_price)
+      expect(items.first[:unit_price]).to be_a Float
+      expect(items.first).to have_key(:merchant_id)
+      expect(items.first[:merchant_id]).to be_a Integer
+    end
   end
 end
