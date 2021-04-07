a1628e9d7c451ecc44c26a47bade8056a925a8dc
Arika Turing <ablenker@gmail.com>
2021-04-06 18:46:56 -0600

    Add passing code for one mechant happy/sad test

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :merchants, only: [:index, :show]
+      resources :merchants, only: [:index, :show] do
+        get '/items', to: 'merchants/merchant_items#index'
+      end   
       resources :items, only: [:index, :show]
     end
   end
