a6d717d3f4dfc1d628435a1001d0a5b33302bca8
Arika Turing <ablenker@gmail.com>
2021-04-07 21:28:04 -0600

    Add passing code for update item sad path

--- spec/requests/api/v1/items/update_item_spec.rb
+++ spec/requests/api/v1/items/update_item_spec.rb
@@ -21,4 +21,30 @@ RSpec.describe "Update item API" do
       expect(expected[:name]).to eq(new_params[:name])
     end
   end
+  describe 'sad path' do
+    it 'Returns 404 if no item id found' do
+      merchant = create(:merchant)
+      original_item = create(:item, merchant_id: merchant.id)
+
+      new_params = ({
+                  name: 'New item',
+                })
+      patch "/api/v1/items/333", headers: headers, params: JSON.generate(item: new_params)
+
+      expect(response.status).to eq(404)
+      expect(response).to be_not_found
+    end
+
+    it 'Returns 404 if no merchant id found' do
+      merchant = create(:merchant)
+      original_item = create(:item, merchant_id: merchant.id)
+      new_params = ({
+                  merchant_id: 333,
+                })
+      patch "/api/v1/items/333", headers: headers, params: JSON.generate(item: new_params)
+
+      expect(response.status).to eq(404)
+      expect(response).to be_not_found
+    end
+  end
 end
