42996c18f378f2a40baf1145b6e8111412342b04
Arika Turing <ablenker@gmail.com>
2021-04-08 10:36:21 -0600

    Add happy path test for delete item

---
+++ spec/requests/api/v1/items/delete_item_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe "Delete item API" do
+  describe 'happy path' do
+    it 'deletes an item given correct id' do
+      merchant = create(:merchant)
+      item = create(:item, merchant_id: merchant.id)
+
+      expect(Item.count).to eq(1)
+
+      delete "/api/v1/items/#{item.id}"
+
+      expect(response).to be_successful
+      expect(response.status).to eq(202)
+      expect(Item.count).to eq(0)
+      expect{Item.find(item.id)}.to raise_error(ActiveRecord::RecordNotFound)
+    end
+  end
+end 
